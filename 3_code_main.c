4a2b2956e9a001f82d135e9513d8cc49851682d4
박수빈 <87634136+subinbing@users.noreply.github.com>
2022-08-26 15:47:48 +0900

    Add files via upload

---
+++ 3_code_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1 = 10;
+	double num2 = 1.150000;
+
+	printf("오늘은 3일차\n");
+	printf("정수형 : %d\n", num1);
+	printf("실수형 : %lf\n", num2);
+	printf("문자 : %s", "hello");
+
+	return 0;
+}
