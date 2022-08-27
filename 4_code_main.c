ed14638ca742f9bca77c759230af5b6cbe97d2f3
박수빈 <87634136+subinbing@users.noreply.github.com>
2022-08-27 23:11:56 +0900

    Add files via upload

---
+++ 4_code_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a = 5;
+
+	printf("%d\n", a == 5);
+	printf("%d\n", a == 3 + 2);
+	printf("%d\n", a == 5 * 1);
+	printf("%d\n", 1 && 1);
+
+	return 0;
+}
+
+
+
